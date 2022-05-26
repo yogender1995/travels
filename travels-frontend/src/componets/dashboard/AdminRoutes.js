da7222ea48e9f72fd181ccb6481ffc4827cb1993
Vikas Thakur <thakurvikas437@gmail.com>
2022-05-26 11:37:10 +0530

    commiteed
    Commit

--- travels-frontend/src/componets/dashboard/AdminRoutes.js
+++ travels-frontend/src/componets/dashboard/AdminRoutes.js
@@ -3,11 +3,16 @@ import AdminHeader from "./Header";
 import AdminFooter from './Footer';
 import AdminSidebar from './Sidebar';
 import AdminHome from './Home';
+import { Route,Routes } from "react-router";
 
 
 function AdminRoutes() {
     return (
         <div>
+            <Routes>
+            <Route path="/admin" element={<AdminHome />}>
+                </Route>
+            </Routes>
             <AdminHeader />
             <section className="dashboard gray-bg padd-0 mrg-top-50">
                 <div className="container-fluid">
---
+++ travels-frontend/src/componets/dashboard/Hotel.js
@@ -0,0 +1,321 @@
+import React from 'react';
+
+const Hotel = () => {
+    return (
+        <div>
+          <div>
+  <div className="col-md-4 col-sm-6">
+    <article className="hotel-box style-1">
+      <div className="hotel-box-image">
+        <figure>
+          <a href="hotel-detail.html">
+
+
+            <img src="assets/img/hotel/hotel-4.jpg" className="img-responsive listing-box-img" />
+            <div className="list-overlay" />
+            <div className="read_more"><span>Read more</span></div>
+          </a>
+          <div className="discount-flick">-12%</div>
+          <h4 className="hotel-place">
+            <a href="#">Qatar</a>
+          </h4>
+        </figure>
+      </div>
+      <div className="entry-meta">
+        <div className="meta-item meta-rating">
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star-half" />
+        </div>
+        <div className="meta-item meta-comment fl-right">
+          <span className="real-price padd-l-10">From $580/Person</span>
+        </div>
+      </div>
+      <div className="hotel-detail-box">
+        <div className="hotel-ellipsis">
+          <h4 className="hotel-name">
+            <a href="hotel-detail.html">Vision Motel</a>
+          </h4>
+          <p>Proin mi nisi, ultrices eget dictum a, volutpat at risus. Aliquam elementum.</p>
+        </div>
+      </div>
+      <div className="hotel-inner inner-box">
+        <div className="box-inner-ellipsis">
+          <div className="hotel-review entry-location">
+            <span className="review-status bg-success"><i className="ti-check" /></span>
+            <h6><span className="cl-success font-bold">Good</span>1587 Review</h6>
+          </div>
+          <div className="view-box">
+            <div className="fl-right">
+              <span><i className="ti-eye padd-r-5" />658</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+  {/* Single Hotel */}
+  <div className="col-md-4 col-sm-6">
+    <article className="hotel-box style-1">
+      <div className="hotel-box-image">
+        <figure>
+          <a href="hotel-detail.html">
+            <img src="assets/img/hotel/hotel-5.jpg" className="img-responsive listing-box-img"  />
+            <div className="list-overlay" />
+            <div className="read_more"><span>Read more</span></div>
+          </a>
+          <div className="discount-flick">-20%</div>
+          <h4 className="hotel-place">
+            <a href="#">Singapore</a>
+          </h4>
+        </figure>
+      </div>
+      <div className="entry-meta">
+        <div className="meta-item meta-rating">
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star-half" />
+        </div>
+        <div className="meta-item meta-comment fl-right">
+          <span className="real-price padd-l-10">From $910/Person</span>
+        </div>
+      </div>
+      <div className="hotel-detail-box">
+        <div className="hotel-ellipsis">
+          <h4 className="hotel-name">
+            <a href="hotel-detail.html">Monsoon Motel</a>
+          </h4>
+          <p>Proin mi nisi, ultrices eget dictum a, volutpat at risus. Aliquam elementum.</p>
+        </div>
+      </div>
+      <div className="hotel-inner inner-box">
+        <div className="box-inner-ellipsis">
+          <div className="hotel-review entry-location">
+            <span className="review-status bg-danger"><i className="ti-check" /></span>
+            <h6><span className="cl-danger font-bold">Poor</span>870 Review</h6>
+          </div>
+          <div className="view-box">
+            <div className="fl-right">
+              <span><i className="ti-eye padd-r-5" />540</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+  {/* Single Hotel */}
+  <div className="col-md-4 col-sm-6">
+    <article className="hotel-box style-1">
+      <div className="hotel-box-image">
+        <figure>
+          <a href="hotel-detail.html">
+            <img src="assets/img/hotel/hotel-6.jpg" className="img-responsive listing-box-img"  />
+            <div className="list-overlay" />
+            <div className="read_more"><span>Read more</span></div>
+          </a>
+          <div className="discount-flick">-15%</div>
+          <h4 className="hotel-place">
+            <a href="#">Switzerland</a>
+          </h4>
+        </figure>
+      </div>
+      <div className="entry-meta">
+        <div className="meta-item meta-rating">
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star-half" />
+        </div>
+        <div className="meta-item meta-comment fl-right">
+          <span className="real-price padd-l-10">From $640/Person</span>
+        </div>
+      </div>
+      <div className="hotel-detail-box">
+        <div className="hotel-ellipsis">
+          <h4 className="hotel-name">
+            <a href="hotel-detail.html">Comfort Motel</a>
+          </h4>
+          <p>Proin mi nisi, ultrices eget dictum a, volutpat at risus. Aliquam elementum.</p>
+        </div>
+      </div>
+      <div className="hotel-inner inner-box">
+        <div className="box-inner-ellipsis">
+          <div className="hotel-review entry-location">
+            <span className="review-status bg-warning"><i className="ti-check" /></span>
+            <h6><span className="cl-warning font-bold">Fair</span>1422 Review</h6>
+          </div>
+          <div className="view-box">
+            <div className="fl-right">
+              <span><i className="ti-eye padd-r-5" />782</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+  {/* Single Hotel */}
+  <div className="col-md-4 col-sm-6">
+    <article className="hotel-box style-1">
+      <div className="hotel-box-image">
+        <figure>
+          <a href="hotel-detail.html">
+            <img src="public/assets/img/hotel/hotel-7.jpg" className="img-responsive listing-box-img"  />
+            <div className="list-overlay" />
+            <div className="read_more"><span>Read more</span></div>
+          </a>
+          <div className="discount-flick">-14%</div>
+          <h4 className="hotel-place">
+            <a href="#">San Marino</a>
+          </h4>
+        </figure>
+      </div>
+      <div className="entry-meta">
+        <div className="meta-item meta-rating">
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star-half" />
+        </div>
+        <div className="meta-item meta-comment fl-right">
+          <span className="real-price padd-l-10">From $810/Person</span>
+        </div>
+      </div>
+      <div className="hotel-detail-box">
+        <div className="hotel-ellipsis">
+          <h4 className="hotel-name">
+            <a href="hotel-detail.html">Farmhouse Hotel</a>
+          </h4>
+          <p>Proin mi nisi, ultrices eget dictum a, volutpat at risus. Aliquam elementum.</p>
+        </div>
+      </div>
+      <div className="hotel-inner inner-box">
+        <div className="box-inner-ellipsis">
+          <div className="hotel-review entry-location">
+            <span className="review-status bg-info"><i className="ti-check" /></span>
+            <h6><span className="cl-info font-bold">Superb</span>1024 Review</h6>
+          </div>
+          <div className="view-box">
+            <div className="fl-right">
+              <span><i className="ti-eye padd-r-5" />568</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+  {/* Single Hotel */}
+  <div className="col-md-4 col-sm-6">
+    <article className="hotel-box style-1">
+      <div className="hotel-box-image">
+        <figure>
+          <a href="hotel-detail.html">
+            <img src="assets/img/hotel/hotel-8.jpg" className="img-responsive listing-box-img"  />
+            <div className="list-overlay" />
+            <div className="read_more"><span>Read more</span></div>
+          </a>
+          <div className="discount-flick">-10%</div>
+          <h4 className="hotel-place">
+            <a href="#">United States</a>
+          </h4>
+        </figure>
+      </div>
+      <div className="entry-meta">
+        <div className="meta-item meta-rating">
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star-half" />
+        </div>
+        <div className="meta-item meta-comment fl-right">
+          <span className="real-price padd-l-10">From $510/Person</span>
+        </div>
+      </div>
+      <div className="hotel-detail-box">
+        <div className="hotel-ellipsis">
+          <h4 className="hotel-name">
+            <a href="hotel-detail.html">Ivory House Hotel</a>
+          </h4>
+          <p>Proin mi nisi, ultrices eget dictum a, volutpat at risus. Aliquam elementum.</p>
+        </div>
+      </div>
+      <div className="hotel-inner inner-box">
+        <div className="box-inner-ellipsis">
+          <div className="hotel-review entry-location">
+            <span className="review-status bg-success"><i className="ti-check" /></span>
+            <h6><span className="cl-success font-bold">Good</span>256 Review</h6>
+          </div>
+          <div className="view-box">
+            <div className="fl-right">
+              <span><i className="ti-eye padd-r-5" />652</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+  {/* Single Hotel */}
+  <div className="col-md-4 col-sm-6">
+    <article className="hotel-box style-1">
+      <div className="hotel-box-image">
+        <figure>
+          <a href="hotel-detail.html">
+            <img src="assets/img/hotel/hotel-9.jpg" className="img-responsive listing-box-img"  />
+            <div className="list-overlay" />
+            <div className="read_more"><span>Read more</span></div>
+          </a>
+          <div className="discount-flick">-07%</div>
+          <h4 className="hotel-place">
+            <a href="#">Saudi Arabia</a>
+          </h4>
+        </figure>
+      </div>
+      <div className="entry-meta">
+        <div className="meta-item meta-rating">
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star" />
+          <i className="fa fa-star-half" />
+        </div>
+        <div className="meta-item meta-comment fl-right">
+          <span className="real-price padd-l-10">From $570/Person</span>
+        </div>
+      </div>
+      <div className="hotel-detail-box">
+        <div className="hotel-ellipsis">
+          <h4 className="hotel-name">
+            <a href="hotel-detail.html">Prophecy Motel</a>
+          </h4>
+          <p>Proin mi nisi, ultrices eget dictum a, volutpat at risus. Aliquam elementum.</p>
+        </div>
+      </div>
+      <div className="hotel-inner inner-box">
+        <div className="box-inner-ellipsis">
+          <div className="hotel-review entry-location">
+            <span className="review-status bg-info"><i className="ti-check" /></span>
+            <h6><span className="cl-info font-bold">Superb</span>1252 Review</h6>
+          </div>
+          <div className="view-box">
+            <div className="fl-right">
+              <span><i className="ti-eye padd-r-5" />582</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
+
+            
+        </div>
+    );
+}
+
+export default Hotel;
